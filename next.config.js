100e743ad8fb48439a8e2f6093ec9b0ff5b13949
Ashwin Temkar <ashwintemkar@gmail.com>
2025-01-21 09:00:31 +0530

    updated package and dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "node-loader",
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
   
